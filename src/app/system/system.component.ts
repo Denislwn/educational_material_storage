03e42b640065cccf58a3ce1794032950bd478bca
Denis Kharlamov <dharlamov97@gmail.com>
2018-05-06 15:49:35 +0300

    modified nav-bar menu

--- src/app/system/system.component.ts
+++ src/app/system/system.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-system',
@@ -6,17 +6,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./system.component.css']
 })
 export class SystemComponent implements OnInit {
+  isPhone: boolean;
+  @ViewChild('navList') navList: ElementRef;
+  @ViewChild('appPage') appPage;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
+    this.checkDevice();
+  }
+
+  checkDevice() {
+    if (this.appPage.nativeElement.clientWidth < 768) {
+      this.isPhone = true;
+    } else {
+      this.isPhone = false;
+    }
   }
 
   phoneMenu() {
-    let arr = document.getElementsByTagName('li');
-    console.log(arr[0]);
-    for (let i = 0; i < arr.length; i++) {
-      arr[i].className += ' menu-class';
+    const arr = this.navList.nativeElement.className.split(' ');
+    if (arr[arr.length - 1] === 'menu-class') {
+      this.navList.nativeElement.className = '';
+      for (let i = 0; i < arr.length - 1; i++) {
+        if (i < arr.length - 2) {
+          arr[i] += ' ';
+        }
+        this.navList.nativeElement.className += arr[i];
+      }
+    } else {
+      this.navList.nativeElement.className += ' menu-class';
+    }
+  }
+
+  clickMenuElement() {
+    this.checkDevice();
+    if (this.isPhone) {
+      const arr = this.navList.nativeElement.className.split(' ');
+      this.navList.nativeElement.className = '';
+      for (let i = 0; i < arr.length - 1; i++) {
+        if (i < arr.length - 2) {
+          arr[i] += ' ';
+        }
+        this.navList.nativeElement.className += arr[i];
+      }
     }
   }
 
